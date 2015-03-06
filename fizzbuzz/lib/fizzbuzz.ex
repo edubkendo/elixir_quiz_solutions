0ee30de561ee98a3457fa80ea3aa006e9cab7133
Eric West <esw9999@gmail.com>
2015-03-05 21:01:45 -0600

    Completed FizzBuzz solution.

--- fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/lib/fizzbuzz.ex
@@ -1,2 +1,20 @@
 defmodule Fizzbuzz do
+  def up_to(n) do
+    1..n
+    |> Enum.to_list
+    |> Enum.map(&transform/1)
+    |> Enum.join(" ")
+  end
+
+  def transform(num) when rem(num, 15) == 0 do
+    "FizzBuzz"
+  end
+  def transform(num) when rem(num, 3) == 0 do
+    "Fizz"
+  end
+  def transform(num) when rem(num, 5) == 0 do
+    "Buzz"
+  end
+  def transform(num), do: to_string num
 end
+
--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -1,8 +1,33 @@
 defmodule FizzbuzzTest do
   use ExUnit.Case
+  import Fizzbuzz
+
   doctest Fizzbuzz
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "divided by 3 is Fizz" do
+    assert transform(3) == "Fizz"
+    assert transform(6) == "Fizz"
+    assert transform(9) == "Fizz"
+  end
+
+  test "divided by 5 is Buzz" do
+    assert transform(5) == "Buzz"
+    assert transform(10) == "Buzz"
+  end
+
+  test "divided by 3 and 5 is FizzBuzz" do
+    assert transform(15) == "FizzBuzz"
+  end
+
+  test "everything else is a number" do
+    assert transform(1) == "1"
+    assert transform(2) == "2"
+    assert transform(4) == "4"
+    assert transform(7) == "7"
+    assert transform(16) == "16"
+  end
+
+  test "stream up to a number, transforming as you go" do
+    assert up_to(15) == "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
   end
 end
